f7325f46df221e094454fadca19d99d6d374aaee
TropZZ <luiswagner8@gmail.com>
2018-12-03 17:34:23 -0200

    exercicios anteriores do prolog1.pl

--- codigo.pl
+++ codigo.pl
@@ -3,3 +3,12 @@ duplica([X|C], [X,X|D]) :- duplica(C,D).
 
 igual([], []) :- true.
 igual([X|C], [Y|D]):- X == Y, igual(C, D). 
+
+tamanho([], 0).
+tamanho([X|C], T) :- tamanho(C, TC), T is TC+1.
+
+separa([], [], []).
+separa([H|C], [H|P], N,) :- H>=0, separa(C, P, N), !.
+separa([H|C], P, [H|N]) :- H<0, separa(C, P, N), !.
+
+segundo([H,O|T], O).
