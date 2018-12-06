e6aeed9f349de67b9b271451c655431dea4bbac5
TropZZ <luiswagner8@gmail.com>
2018-12-06 14:13:48 -0200

    cola da prova de plp kk

--- codigo.pl
+++ codigo.pl
@@ -19,15 +19,30 @@ ultimo([_|C], Y):- ultimo(C, Y).
 soma([], 0).
 soma([X|T], S):- soma(T, ST), S is ST+X.
 
-fatorial(0, 1) :- !. 
-fatorial(N, F) :- N > 0 ,M is N - 1, fatorial(M, F1), F is F1*N.
-
 contiguo([]):- false, !.
 contiguo([X,X|_]):- true, !.
 contiguo([_|T]):- contiguo(T).
 
-particiona([], L1, L2).
-particona([X], [L1|X], L2).
-particona([X,Y], [L1|X], [L2|Y]).
-particiona([X,Y,Z], [L1|X,Z], [L2|Y]).
-particiona([X,Y|T], L1, L2):- particiona(T, [L1|X], [L2|Y]).
+fatorial(0, 1):- !.
+fatorial(N,_) :- N < 0.
+fatorial(N, F) :- M is N - 1, fatorial(M, F1), F is F1*N.
+
+produto(X,X,X).
+produto(X,Y,R):- X\==Y, Y1 is Y-1,produto(X,Y1,R), R is Y1*Y.
+
+conc([],L,L).
+conc([X|L1],L2,[X|L3]):- conc(L1,L2,L3).
+inverter([],[]).
+inverter([X|Y],Z):- inverter(Y,Y1), conc(Y1,[X],Z).
+
+enesimo(1,X,[X|_]).
+enesimo(N,X,[_|Y]):- enesimo(M,X,Y), N is M + 1.
+
+permutar([],[]).
+permutar([X|L],P):- permutar(L,L1), inserir(X,L1,P).
+
+remover(X,[X|C],C).
+remover(X,[Y|C],[Y|D]):- remover(X,C,D).
+
+insere(X,[X|_]).
+insere(X,[_|Y]):- insere(X,Y). 
